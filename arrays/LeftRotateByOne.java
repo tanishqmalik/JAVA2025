5f7966ddc9925206032a6be723d0c7d051163050
tanishqmalik <jobfortanishq@icloud.com>
2025-01-08 00:00:45 +0530

    arrays

--- arrays/LeftRotateByOne.java
+++ arrays/LeftRotateByOne.java
@@ -1,9 +1,4 @@
 
-import java.util.Scanner;
-
-// import java.util.Scanner;
-
-// public class LeftRotateByOne{
 //     public static void main(String args[]){
 //         Scanner sc = new Scanner(System.in);
 
@@ -31,27 +26,51 @@ import java.util.Scanner;
 // }
 
 
-public class LeftRotateByOne {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+// public class LeftRotateByOne {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
 
-        int limit = sc.nextInt();
-        int arr[] = new int[limit];
+//         int limit = sc.nextInt();
+//         int arr[] = new int[limit];
 
-        for(int i=0; i<limit; i++){
-            arr[i] = sc.nextInt();
-        }
+//         for(int i=0; i<limit; i++){
+//             arr[i] = sc.nextInt();
+//         }
 
-        int first = arr[0];
+//         int first = arr[0];
 
-        for(int i=1; i<arr.length; i++){
-            arr[i-1] = arr[i];
-        }
-        arr[arr.length-1]=first;
+//         for(int i=1; i<arr.length; i++){
+//             arr[i-1] = arr[i];
+//         }
+//         arr[arr.length-1]=first;
 
 
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-}
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+// 5
+// 1 2 3 4 5
+// 2 3 4 5 1 
+
+
+// public class LeftRotateByOne {
+//     public static void main(String[] args) {
+//         int arr[] = {2,4,5,6};
+
+//         int first = arr[0];
+
+//         for(int i=1; i<arr.length; i++){
+//             arr[i-1]=arr[i];
+//         }
+
+//         arr[arr.length-1] = first;
+
+//         for(int i=0; i<arr.length; i++){
+//             System.err.println(arr[i]);
+//         }
+
+//     }
+// }
--- arrays/RotateByKEle.java
+++ arrays/RotateByKEle.java
@@ -1,30 +1,51 @@
 
-import java.util.Scanner;
+//     public static void reverse( int arr[], int start, int end){
+
+//         while(start<end){
+//             int temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end]= temp;
+//             start++;
+//             end--;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int limit = sc.nextInt();
+
+//         int arr[] = new int[limit];
+
+//         for(int i=0; i<limit; i++){
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int k = sc.nextInt();
+
+//         reverse(arr, 0, arr.length-k-1);
+//         reverse(arr, arr.length-k, arr.length-1);
+//         reverse(arr, 0, arr.length-1);
+
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+// }
+
 
 public class RotateByKEle {
 
-    public static void reverse( int arr[], int start, int end){
-
+    static void reverse(int arr[], int start, int end){
         while(start<end){
             int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end]= temp;
+            arr[start]=arr[end];
+            arr[end]=temp;
             start++;
             end--;
         }
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int limit = sc.nextInt();
 
-        int arr[] = new int[limit];
-
-        for(int i=0; i<limit; i++){
-            arr[i] = sc.nextInt();
-        }
-
-        int k = sc.nextInt();
-
+    static void right(int arr[],int k){
         reverse(arr, 0, arr.length-k-1);
         reverse(arr, arr.length-k, arr.length-1);
         reverse(arr, 0, arr.length-1);
@@ -32,6 +53,38 @@ public class RotateByKEle {
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
-        System.out.println();
+
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, arr.length-k, arr.length-1);
+        reverse(arr, 0, arr.length-k-1);
+        
+    }
+
+
+    static void left(int arr[],int k){
+        reverse(arr, 0, k-1);
+        reverse(arr, k, arr.length-1);
+        reverse(arr, 0, arr.length-1);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+       
+
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, arr.length-k, arr.length-1);
+        reverse(arr, 0, arr.length-k-1);
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+
+        int k=2;
+
+        right(arr,k);
+        System.err.println();
+        left(arr,k);
+        
     }
 }
