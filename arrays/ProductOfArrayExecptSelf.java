4ab52adb3d6741cbfe19678233bebb2ada5d531a
tanishqmalik <jobfortanishq@icloud.com>
2025-01-13 18:47:10 +0530

    productofarrayitself

--- arrays/ProductOfArrayExecptSelf.java
+++ arrays/ProductOfArrayExecptSelf.java
@@ -23,3 +23,40 @@
 //         }
 //     }
 // }
+
+// 13jan
+public class ProductOfArrayExecptSelf {
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3,4};
+
+        int ans[] = new int[arr.length];
+        int ans2[] = new int[arr.length];
+
+        int piche =1;
+
+        for(int i=0; i<arr.length; i++){
+            ans[i] = piche;
+            piche = arr[i]*piche;
+        }
+
+        int aage = 1;
+
+        for(int i=arr.length-1; i>=0; i--){
+            ans2[i] = aage;
+            aage = arr[i]*aage;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            ans[i] = ans[i]*ans2[i];
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(ans[i]);
+        }
+
+
+
+
+    }
+}
