174269d5ebc48359bd296519a7e11cbea26c18ee
tanishqmalik <jobfortanishq@icloud.com>
2025-01-19 19:00:32 +0530

    okay

---
+++ arrays/leetcode/plusone.java
@@ -0,0 +1,54 @@
+
+// import java.lang.reflect.Array;
+// import java.util.ArrayList;
+import java.util.*;
+// package arrays.leetcode;
+
+public class plusone {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+
+
+        ArrayList <Integer> list = new ArrayList<>();
+
+        int number = 0;
+
+        for(int digit : arr){
+            number = number *10 + digit;
+        }
+
+        // System.out.println(number);
+
+        number = number+1;
+
+
+        while(number>0){
+            int digit = number%10;
+            // System.out.println(digit);
+            list.add(digit);
+            number = number/10;
+        }
+
+        Collections.reverse(list);
+
+        int result[] = new int[list.size()];
+
+        int i=0;
+
+        for(int x : list){
+            result[i]= list.get(i);
+            i++;
+        }
+
+        // for(int x : arr){
+        //     System.out.println(x);
+        // }
+
+        
+
+        
+
+        // System.out.println(number);
+
+    }
+}
