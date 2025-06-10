db1218ba3027526688b266d51bf243bb22913706
tanishqmalik <jobfortanishq@icloud.com>
2025-06-11 00:20:22 +0530

    ok

--- 8June2025/RSquareSoft4LPA/MaxCharFrequency.java
+++ 8June2025/RSquareSoft4LPA/MaxCharFrequency.java
@@ -8,17 +8,47 @@ import java.util.*;
 // package 8June2025.RSquareSoft4LPA;
 
 public class MaxCharFrequency {
-    public static void main(String args[]){
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     String input = sc.nextLine();
+
+    //     HashMap<Character,Integer> freqMap = new HashMap<>();
+
+    //     for(char c : input.toCharArray()){
+    //         if(c==' '){
+    //             continue;
+    //         }
+    //         freqMap.put(c, freqMap.getOrDefault(c,0)+1);
+    //     }
+
+    //     int maxFreq = Collections.max(freqMap.values());
+
+    //     System.out.println(maxFreq);
+
+    //     for(Map.Entry<Character,Integer> entry : freqMap.entrySet()){
+    //         if(entry.getValue() == maxFreq){
+    //             System.out.println(entry.getKey());
+    //             break;
+    //         }
+    //     }
+    // }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String input = sc.nextLine();
 
         HashMap<Character,Integer> freqMap = new HashMap<>();
+
 
         for(char c : input.toCharArray()){
             if(c==' '){
                 continue;
             }
-            freqMap.put(c, freqMap.getOrDefault(c,0)+1);
+
+            freqMap.put(c, freqMap.getOrDefault(c,0) +1);
+
+
         }
 
         int maxFreq = Collections.max(freqMap.values());
@@ -31,6 +61,7 @@ public class MaxCharFrequency {
                 break;
             }
         }
+
     }
 
     
--- 8June2025/RSquareSoft4LPA/ReverseTheString.java
+++ 8June2025/RSquareSoft4LPA/ReverseTheString.java
@@ -5,26 +5,48 @@ import java.util.*;
 
 public class ReverseTheString {
 
-    public static void main(String args[]){
+    // public static void main(String args[]){
         
     
-    Scanner sc = new Scanner(System.in);
+    // Scanner sc = new Scanner(System.in);
 
-    String input = sc.nextLine();
+    // String input = sc.nextLine();
 
-    String[] words = input.split(" ");
+    // String[] words = input.split(" ");
 
-    StringBuilder reversed = new StringBuilder();
+    // StringBuilder reversed = new StringBuilder();
 
-    for(int i = words.length-1; i>=0; i--){
-        reversed.append(words[i]);
+    // for(int i = words.length-1; i>=0; i--){
+    //     reversed.append(words[i]);
 
-        if(i!=0){
-            reversed.append(" ");
+    //     if(i!=0){
+    //         reversed.append(" ");
+    //     }
+
+    // }
+
+    // System.out.println(reversed);
+    // }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        String[] words = input.split(" ");
+
+        StringBuilder reversed = new StringBuilder();
+
+        for(int i=words.length-1; i>=0; i--){
+            reversed.append(words[i]);
+
+            if(i!=0){
+                reversed.append(" ");
+            }
         }
 
-    }
+        System.out.println(reversed);
 
-    System.out.println(reversed);
     }
 }
